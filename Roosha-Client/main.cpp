5fc517c054cc1360a78592f26a1c844dabe04350
Alex <adkosmachev@edu.hse.ru>
2016-10-21 16:49:15 +0300

    Get rid of deamon define
    Right now, it only desturbs us

--- Roosha-Client/main.cpp
+++ Roosha-Client/main.cpp
@@ -1,21 +1,8 @@
-
-
-//#define DAEMON // uncomment, if you want to run application in daemon mode
-
-
-#if defined DAEMON
-    #include "Core/applicationdaemon.h"
-#else
-    #include <QApplication>
-    #include "Core/centralcontroller.h"
-#endif
+#include <QApplication>
+#include "Core/centralcontroller.h"
 
 int main(int argc, char *argv[])
 {
-#if defined DAEMON
-    ApplicationDaemon daemon(argc, argv);  // not confortable to debugging daemon
-    return daemon.exec();
-#else
     QApplication app(argc, argv);
 
     app.setQuitOnLastWindowClosed(false);
@@ -24,11 +11,4 @@ int main(int argc, char *argv[])
     centralController.start();
 
     return app.exec();
-#endif
 }
-
-
-// QML:
-//    QQmlApplicationEngine engine;
-//    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-//#include <QQmlApplicationEngine>
